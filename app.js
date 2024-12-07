e3572bdbc93788179b0b7d24dc056bb03beb51d3
Andy Stuckey <andystuckey@mail.com>
2024-12-06 19:09:59 -0600

    finished the game per the lesson

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const score = document.querySelector('#score')
 let result = 0
 let hitPosition
 let currentTime = 60
+let timerId = null
 
 function randomSquare() {
     squares.forEach(square => {
@@ -29,8 +30,7 @@ squares.forEach(square => {
 })
 
 function moveMole() {
-    let timerId = null
-    timerId = setInterval(randomSquare, 1000)
+    timerId = setInterval(randomSquare, 500)
 }
 
 moveMole()
@@ -41,6 +41,7 @@ function countDown() {
 
     if(currentTime == 0) {
         clearInterval(countDownTimerId)
+        clearInterval(timerId)
         alert('GAME OVER! Your final score is ' + result)
     }
 }
@@ -49,4 +50,3 @@ let countDownTimerId = setInterval(countDown, 1000)
 
 
 
-// timestamp: 24:30
